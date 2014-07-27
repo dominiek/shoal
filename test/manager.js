095947c03e08b25f74b4c49f14c35d3894b54b5e
Dominiek ter Heide <info@dominiek.com>
2014-07-27 22:13:51 +0200

    Renamed some commands

--- test/manager.js
+++ test/manager.js
@@ -6,8 +6,8 @@ describe('Manager', function(){
 
   it('should run processes and keep track of those', function(){
     var manager = new Manager();
-    manager.run('ping', ['localhost'], {env: {TESTVAR: "123"}});
-    var processes = manager.list();
+    manager.runProcess('ping', ['localhost'], {env: {TESTVAR: "123"}});
+    var processes = manager.listProcesses();
     var pids = Object.keys(processes);
     var process = processes[pids[0]];
     assert.equal(pids.length, 1);
@@ -19,13 +19,13 @@ describe('Manager', function(){
 
   it('should allow us to kill processes', function(){
     var manager = new Manager();
-    manager.run('ping', ['localhost'], {env: {TESTVAR: "123"}});
-    var processes = manager.list();
+    manager.runProcess('ping', ['localhost'], {env: {TESTVAR: "123"}});
+    var processes = manager.listProcesses();
     var pids = Object.keys(processes);
     var process = processes[pids[0]];
     assert.equal(pids.length, 1);
-    manager.kill(pids[0]);
-    assert.equal(Object.keys(manager.list()).length, 0);
+    manager.killProcess(pids[0]);
+    assert.equal(Object.keys(manager.listProcesses()).length, 0);
   });
 
 });
