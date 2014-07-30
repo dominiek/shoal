5cfe8f4f0274405f3add400979c73e89333ea2e2
Dominiek ter Heide <info@dominiek.com>
2014-07-30 16:59:56 +0200

    Simple client-server

---
+++ test/client.js
@@ -0,0 +1,52 @@
+
+var assert = require('chai').assert;
+var async = require('async');
+
+var Manager = require('../lib/manager');
+var Server = require('../lib/server');
+var Client = require('../lib/client');
+
+var server = new Server(new Manager());
+server.start();
+
+describe('Client', function(){
+
+  it('Should allow us to deploy a process configuration', function(done){
+    var configuration = {
+      processes: [
+        {
+          name: 'Ping Localhost',
+          cmd: 'ping',
+          args: ['localhost'],
+          instances: 3
+        }
+      ]
+    };
+
+    var client = new Client();
+
+    async.series([
+      function(next) {
+        client.listInstances(function(err, result) {
+          assert.equal(err, null);
+          assert.equal(0, Object.keys(result).length);
+          next();
+        });
+      },
+      function(next) {
+        client.deploy(configuration, function(err, result) {
+          assert.equal(err, null);
+          next();
+        });
+      },
+      function(next) {
+        client.listInstances(function(err, result) {
+          assert.equal(err, null);
+          assert.equal(3, Object.keys(result).length);
+          next();
+        });
+      }
+    ], done);
+  });
+
+});
